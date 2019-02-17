160b23b07354acdc37b02fef445e49f4d8745107
Olavur Ellefsen <oe@tokni.com>
2019-02-17 13:47:28 +0000

    Resize placeholder image

--- src/PuzzleItems/DragItems/DragItems.js
+++ src/PuzzleItems/DragItems/DragItems.js
@@ -3,7 +3,7 @@ import { DragItemsContainer, DragItemsImage } from "./DragItems.style";
 
 const DragItems = () => (
   <DragItemsContainer>
-    <DragItemsImage src={"https://via.placeholder.com/200x800?text=DragItems"} />
+    <DragItemsImage src={"https://via.placeholder.com/400x800?text=DragItems"} />
   </DragItemsContainer>
 );
 
