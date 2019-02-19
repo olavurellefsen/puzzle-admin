5f220ffc0c1d82da0bb35df97e7ee63910e43a8a
Olavur Ellefsen <oe@tokni.com>
2019-02-19 08:26:12 +0000

    Get scenes via database subscription

--- src/SceneList/SceneList.js
+++ src/SceneList/SceneList.js
@@ -1,16 +1,39 @@
 import React from "react";
 import Scene from "./Scene/Scene";
 import { SceneListContainer, SceneListBox } from "./SceneList.style";
-import sceneData from "./SceneData";
+import { Subscription } from "react-apollo";
+import gql from "graphql-tag";
 
 const SceneList = () => {
-  const scenes = sceneData.data.scene;
+  const subscription = gql`
+    subscription {
+      scene(order_by: { sequence: asc }) {
+        id
+        title
+        image_filename
+        initial_puzzle_id
+        sequence
+      }
+    }
+  `;
+
   return (
     <SceneListContainer>
       <SceneListBox>
-        {scenes.map((scene, index) => {
-          return <Scene key={index} scene={scene} />;
-        })}
+      <Subscription subscription={subscription}>
+          {({ data, loading }) => {
+            if (loading) {
+              return "Loading...";
+            } else {
+              let scenes = data.scene;
+              return(
+                scenes.map((scene, index) => {
+                  return <Scene key={index} scene={scene} />;
+                })
+              );
+            }
+          }}
+        </Subscription>
       </SceneListBox>
     </SceneListContainer>
   );
--- src/SceneList/SceneList.test.js
+++ src/SceneList/SceneList.test.js
@@ -1,7 +1,6 @@
 import React from "react";
 import { shallow } from "enzyme";
 import SceneList from "./SceneList";
-import Scene from "./Scene/Scene";
 
 describe("SceneList", () => {
   let wrapper;
@@ -14,8 +13,5 @@ describe("SceneList", () => {
   it("should render correctly", () => expect(wrapper).toMatchSnapshot());
   it("should render a SceneListContainer", () => {
     expect(wrapper.find("SceneListContainer").length).toEqual(1);
-  });
-  it("should render the Scene component", () => {
-    expect(wrapper.containsMatchingElement(<Scene />)).toEqual(true);
   });
 });
