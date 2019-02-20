07164ab5c8dbbaab7662ec6cf98c794e1a08bc1c
Olavur Ellefsen <oe@tokni.com>
2019-02-20 22:38:25 +0000

    Update test to use react-testing-library instead of Enzyme

--- src/PuzzleList/PuzzleList.test.js
+++ src/PuzzleList/PuzzleList.test.js
@@ -1,37 +1,16 @@
 import React from "react";
-import { shallow, mount } from "enzyme";
-import MainContextStore from "../MainContextStore";
+import { render, cleanup, waitForElement } from 'test-utils';
 import PuzzleList from "./PuzzleList";
-import { ApolloProvider } from "react-apollo";
-import client from "../heyBreydLocalizationClient";
+afterEach(cleanup)
 
-describe("PuzzleList", () => {
-  let wrapper;
-  beforeEach(
-    () =>
-      (wrapper = shallow(
-        <MainContextStore>
-          <PuzzleList />
-        </MainContextStore>
-      ))
-  );
-  it("should render correctly", () => expect(wrapper).toMatchSnapshot());
+it("should render correctly", () => {
+  const { container } = render(<PuzzleList />);
+  expect(container).toMatchSnapshot();
 });
 
-describe("mounted PuzzleList", () => {
-  let wrapper;
-  beforeEach(
-    () =>
-      (wrapper = mount(
-        <ApolloProvider client={client}>
-          <MainContextStore>
-            <PuzzleList />
-          </MainContextStore>
-        </ApolloProvider>
-      ))
-  );
-
-  it("should render a PuzzleListContainer", () => {
-    expect(wrapper.find("PuzzleListContainer").length).toEqual(1);
-  });
+it("should render a puzzle list", async () => {
+  const { debug, getByText } = render(<PuzzleList />);
+  debug();
+  expect(getByText("Loading...")).toBeTruthy();
+  await waitForElement(() => getByText('AUDIOFILE'));
 });
