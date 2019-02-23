a9b8bde96e81563b6804eb4348a44a8b815266d4
Olavur Ellefsen <oe@tokni.com>
2019-02-23 11:30:48 +0000

    Refactor to use => instead of class

--- src/Callback/Callback.js
+++ src/Callback/Callback.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react';
-import loading from './loading.svg';
+import React from "react";
+import loading from "./loading.svg";
 
-class Callback extends Component {
-  render() {
-    return (
-      <div>
-        <img src={loading} alt="loading" />
-      </div>
-    );
-  }
+export default () => {
+  return (
+    <div>
+      <img src={loading} alt="loading" />
+    </div>
+  );
 }
-
-export default Callback;
