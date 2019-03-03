3957f3e9b2927e4ab20d17875d1998771a911ee3
Olavur Ellefsen <oe@tokni.com>
2019-03-03 22:11:54 +0000

    Remove DragLogic components

--- src/PuzzleItems/DragLogics/DragLogic/DragLogic.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { DragLogicContainer } from "./DragLogic.style";
-
-export default () => (
-  <DragLogicContainer data-testid="DragLogicContainer" />
-);
--- src/PuzzleItems/DragLogics/DragLogics.style.js
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const DragLogicsContainer = styled.div`
-  margin: 0px 10px;
-`;
-
-export const DragLogicsImage = styled.img``;
