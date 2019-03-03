9b1eeae2b2b03459e0dc6093b0d69cec9df2c701
Olavur Ellefsen <oe@tokni.com>
2019-03-03 12:25:46 +0000

    Update styling

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,15 +1,16 @@
 import React from "react";
 import Scenes from "../Scenes/Scenes";
+import { HomeContainer } from "./Home.style";
 
 export default props => {
   const { isAuthenticated } = props.auth;
   return (
-    <>
+    <HomeContainer>
       {isAuthenticated() && (
         <div data-testid="Scenes">
           <Scenes />
         </div>
       )}
-    </>
+    </HomeContainer>
   );
 }
